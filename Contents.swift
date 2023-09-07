c730a1b380bb60a194479e66aa5d74478e65922c
julialuciani <julia.luciani@warren.com.br>
2023-09-06 21:55:03 -0300

    adding code lines

---
+++ Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+let firstName = "Steve"
+
+var lastName: String? = "Jobs";
+
+print("\(firstName) \(lastName ?? "Wozniak")")
+
+if let lastName {
+    print("\(firstName) \(lastName)")
+}
